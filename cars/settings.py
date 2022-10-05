78139e7fad473017c3868224d3cf2309db6cad97
Komil00 <komiltuev@gmail.com>
2022-10-05 12:33:08 +0500

    first commit

--- cars/settings.py
+++ cars/settings.py
@@ -135,6 +135,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
